c7a386f6cd47de5058b132677e7dfcfd3bfb8ee9
MKondakova <milanon@ya.ru>
2021-02-09 20:42:47 +0300

    add JSDoc

--- source/get.js
+++ source/get.js
@@ -1,14 +1,23 @@
 'use strict';
 
+/**
+ * @description getting nested property of an object
+ * @param {object} object 
+ * @param {string} properties - path to nested object property
+ * @return {object|undefined} property value or undefined if it doesn`t exist
+ */
+
 const get = (object, properties) => {
 	if (typeof object !=='object' || !object || typeof properties !== 'string') {
 		return;
 	}
+
 	let propertiesList = properties.match(/(\.\w+)/g);
 	if (!propertiesList) {
 		return object;
 	}
 	propertiesList = propertiesList.map((str) => str.slice(1));
+
 	for (let i = 0; i < propertiesList.length && object !== undefined; i++) {
 		object = object[propertiesList[i]];
 	}
