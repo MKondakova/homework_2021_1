2d09dfe739cd63285d1ca8b7e150f90843551933
MKondakova <milanon@ya.ru>
2021-02-09 12:05:58 +0300

    add semicolons

--- source/get.js
+++ source/get.js
@@ -1,17 +1,17 @@
-'use strict'
+'use strict';
 
 const get = (object, properties) => {
-  if (properties == null || object == null) {
-    return undefined
-  }
-  properties = properties.match(/(\.\w+)/g)
-  if (properties === null) {
-    return object
-  }
-  properties = properties.map((str) => str.slice(1))
-  let result = object
-  for (let i = 0; i < properties.length && result !== undefined; i++) {
-    result = result[properties[i]]
-  }
-  return result
-}
+	if (properties == null || object == null) {
+		return;
+	}
+	let propertiesList = properties.match(/(\.\w+)/g);
+	if (propertiesList === null) {
+		return object;
+	}
+	propertiesList = propertiesList.map((str) => str.slice(1));
+	let result = object;
+	for (let i = 0; i < propertiesList.length && result !== undefined; i++) {
+		result = result[propertiesList[i]];
+	}
+	return result;
+};
